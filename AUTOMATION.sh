3adc6fc701866a35d29c2c938cf36677371144fd
Lakshmikanth-3110 <68607961+Lakshmikanth-3110@users.noreply.github.com>
2021-03-07 01:01:23 +0530

    Update AUTOMATION.sh

--- AUTOMATION.sh
+++ AUTOMATION.sh
@@ -13,6 +13,9 @@ Log_Directory="/var/log/apache2/"
 timestamp=$(date '+%d%m%Y-%H%M%S')
 Temp_Directory="/tmp/"
 
+HTML_FOLDER="/var/www/html/"
+Inventory_Filename="inventory.html"
+
 echo "Performing an update of the package details and the package list at the start of the script"
 
 sudo apt update -y
@@ -48,12 +51,37 @@ cd $Log_Directory
 
 tar -cvf $TAR_FILE_NAME.tar *.log
 
+size_of_the_file=$(ls -lah $TAR_FILE_NAME.tar | awk '{ print $5}')
+
 cp $TAR_FILE_NAME.tar $Temp_Directory
 
 cd $Temp_Directory
 
 cd $HOME
 
+echo "Copy the File to S3 Bucket"
+
 aws s3 cp /$Temp_Directory/$TAR_FILE_NAME.tar s3://${S3_Bucket}/$TAR_FILE_NAME.tar
+
+echo "Book - Keeping of the Tar File of Logs - Start"
+
+cd $HTML_FOLDER
+if [ -f "$Inventory_Filename" ];
+then 
+    echo "The Directory and the File Exists"
+	cd $HTML_FOLDER
+	echo "" >> $Inventory_Filename
+	echo >> $Inventory_Filename
+	echo "httpd-logs \t ${timestamp}   \t      tar   \t     $size_of_the_file" >> $Inventory_Filename
+else
+    echo "The File is not present"
+	cd $HTML_FOLDER
+	echo "" > $Inventory_Filename
+	echo "Log Type    \t      Time Created    \t     Type    \t    Size" >> $Inventory_Filename
+	echo >> $Inventory_Filename
+	echo "httpd-logs   \t     ${timestamp}    \t     tar    \t     $size_of_the_file" >> $Inventory_Filename
+fi
+
+echo "Book - Keeping of the Tar File of Logs - End"
 
 echo "End of Automation Script"
